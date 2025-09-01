3b1f6773a928a348f564d4659f54a7574a93dc8e
dvaghasiya766 <dvaghasiya766@rku.ac.in>
2025-09-01 11:43:58 +0530

    Final App.tsx

--- App.tsx
+++ App.tsx
@@ -1,45 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// import { StatusBar, useColorScheme, View } from 'react-native';
+// import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+// import Navigation from './src/Navigation/Navigation';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   return (
+//     <SafeAreaProvider style={{ flex: 1 }}>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <SafeAreaView style={{ flex: 1 }}>
+//         <Navigation />
+//       </SafeAreaView>
+//     </SafeAreaProvider>
+//   );
+// }
+
+// export default App;
+import { StatusBar, Text, useColorScheme, View } from 'react-native';
+import Navigation from './src/Navigations/Navigation';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SignInScreen } from './src/Screens';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <>
+      <StatusBar barStyle={'dark-content'} />
+      <Navigation />
+    </>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
